fe7819a5ab25d7f3016a6a6593867d9f8156a51a
icaropereira404 <icarop995@gmail.com>
2023-04-25 17:15:06 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './components/header/index'
+
+function App() {
+
+  return (
+    <div>
+      <h1>kadlaskdçlss</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/header/index.jsx
@@ -0,0 +1,15 @@
+import './style.css'
+import Logo from '../../assets/logo.svg'
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <div className="logo">
+                <img src={Logo} />
+            </div>
+            <nav>
+                <h1>asldkaçl</h1>
+            </nav>
+        </header>
+    )
+}
