690c4a9431e497c5d472bc72e26fe436571bbe11
icaropereira404 <icarop995@gmail.com>
2023-05-02 21:11:43 -0300

    Ajeitando a responsavidade do Sobre nós.

--- src/pages/sections/AboutUs/index.jsx
+++ src/pages/sections/AboutUs/index.jsx
@@ -5,21 +5,26 @@ import Image from '../../../assets/quemsomos.svg'
 const AboutUs = () => {
     return (
         <div className="container">
-        <section className="content">
-            <div className='left'>
-                <h1>
-                sua casa fora de casa.
-                </h1>
-                <p>Seja bem-vindo(a) à Friendly Host, a plataforma que conecta
-                estudantes a famílias dispostas a fornecer moradia temporária em
-                grandes centros. Estamos muito felizes em tê-lo(a) aqui!</p>          
-            </div>
-            <div className='right'>
-                <img src={Image} alt="Icone House" />
-            </div>
-        </section>
-            
-    </div>
+            <section className="content">
+                <div className='left'>
+                    <h1>
+                        quem somos?
+                    </h1>
+                    <p>Somos uma equipe de profissionais comprometidos em
+                        tornar a educação acessível a todos,
+                        independentemente de onde vivem ou de sua situação
+                        financeira. Nós acreditamos que todos merecem a
+                        oportunidade de aprender e crescer, e é por isso que
+                        criamos uma plataforma única que conecta estudantes a
+                        famílias dispostas a fornecer moradia temporária sem
+                        custos em grandes centros.</p>
+                </div>
+                <div className='right'>
+                    <img src={Image} alt="Icone House" />
+                </div>
+            </section>
+
+        </div>
     )
 }
 
